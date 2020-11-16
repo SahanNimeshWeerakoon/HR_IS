eba04a286bda586af7415a764140b50d1f80b3da
Tharindu2413 <CB008632@students.apiit.lk>
2020-11-16 13:20:40 +0530

    Initializing employee

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TestComponent } from './components/test/test.component';
+import { EmployeeComponent } from './component/employee/employee.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    EmployeeComponent
   ],
   imports: [
     BrowserModule,
