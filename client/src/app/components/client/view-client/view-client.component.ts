29fe18a0d9fb709740b35d922479c1f7e2de5ad1
ranuka98 <69889092+ranuka98@users.noreply.github.com>
2020-12-22 11:58:31 +0530

    clientdetails deleted button added

--- client/src/app/components/client/view-client/view-client.component.ts
+++ client/src/app/components/client/view-client/view-client.component.ts
@@ -17,6 +17,17 @@ export class ViewClientComponent implements OnInit {
     private route: ActivatedRoute
   ) { }
 
+  deleteClient(id) {
+    this.clientService.deleteClient(id)
+      .subscribe(resp => {
+        if(resp.success) {
+          this.ngOnInit();
+          console.log('Client Deleted')
+        } else {
+          console.log('Delete client error');
+        }
+      });
+  }
   ngOnInit(): void {
     this.id = this.route.snapshot.paramMap.get('id');
     this.clientService.find(this.id)
